379649ad15afc565b85e1b619cfb10502262787a
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2025-01-07 22:03:26 +0530

    Update hello.py
    Ask user for their name, Remove whitespace from str and Capitalize user's name

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
-# Ask user for their name
-name = input("What is your name? ")
+# Ask user for their name, Remove whitespace from str and Capitalize user's name
+name = input("What is your name? ").strip().title()
 
 # Say hello to user
-print("hello, ",end="")
-print(name)
+print("hello, ",name)
