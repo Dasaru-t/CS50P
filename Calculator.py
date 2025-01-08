1a56a58e603eeed68d3425681ca5934d9990c8d4
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2025-01-08 22:30:33 +0530

    Calculate and print the sum of two integers
    This program prompts the user to input two integers, calculates their sum, and displays the result.

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+# Prompt the user to enter the first integer
+x = int(input("What's x? "))
+
+# Prompt the user to enter the second integer
+y = int(input("What's y? "))
+
+# Calculate the sum of x and y
+z = x + y
+
+# Print the result of the addition
+print(z)
