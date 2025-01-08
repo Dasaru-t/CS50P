51a2a5bf4834b9bd652050cf5f34a754d9475b0b
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2025-01-08 22:16:51 +0530

    Update hello.py
    # Split user's name into first name and last name

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 # Ask user for their name, Remove whitespace from str and Capitalize user's name
 name = input("What is your name? ").strip().title()
 
+# Split user's name into first name and last name
+first, last = name.split(" ")
+
 # Say hello to user
-print("hello, ",name)
+print("hello, ",first)
