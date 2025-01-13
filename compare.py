200be1e857e09d9fe772ef5c3040192b7d558a26
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2025-01-13 23:36:58 +0530

    Compare two integers and print their relationship
    This program takes two integers as input, compares them, and outputs whether the first is less than, greater than, or equal to the second.

---
+++ compare.py
@@ -0,0 +1,16 @@
+# Prompt the user for the first integer input
+x = int(input("What's x? "))
+
+# Prompt the user for the second integer input
+y = int(input("What's y? "))
+
+# Compare the two integers and print the appropriate message
+if x < y:
+    # If x is less than y, print this message
+    print("x is less than y")
+elif x > y:
+    # If x is greater than y, print this message
+    print("x is greater than y")
+else:
+    # If x is equal to y, print this message
+    print("x is equal to y")
