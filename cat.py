e317f75def618676e0c8f886b573df5d2d5d2767
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2025-01-21 22:25:49 +0530

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,8 +1,8 @@
-# Initialize a counter variable i with the value 1
-i = 1
+# Initialize a counter variable i with the value 0
+i = 0
 
 # Use a while loop to print "meow" three times
-while i <= 3:
+while i < 3:
     # Print "meow"
     print("meow")
     # Increment the counter by 1 in each iteration
